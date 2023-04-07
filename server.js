f997289d0a1f418670743de11be1d5127e5bbc8e
anthonyss09 <76715818+anthonyss09@users.noreply.github.com>
2023-04-07 11:34:18 -0400

    first deployment

--- server.js
+++ server.js
@@ -9,15 +9,30 @@ import recipesRoutes from "./routes/recipeRoutes.js";
 
 app.use(express.json());
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+// Accessing the path module
+import path from "path";
+// import client build
+const __dirname = path.dirname(__filename);
+
 //routes
 app.use("/api/auth", authRoutes);
 app.use("/api/recipes", recipesRoutes);
 
-const port = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, "./client/build")));
+// redirect requests to index.html
+app.get("*", function (request, response) {
+  response.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to Barkeeper");
 });
+
+const port = process.env.PORT || 8080;
 
 const start = async () => {
   try {
